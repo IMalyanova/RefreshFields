d283a2a48dfa2767875710735bf9e785b4685ebc
IMalyanova <53153912+IMalyanova@users.noreply.github.com>
2019-08-07 00:10:35 +0300

    готовый

--- RefreshFields/src/Loader.java
+++ RefreshFields/src/Loader.java
@@ -3,7 +3,6 @@ public class Loader {
     public static void main(String[] args)   {
 
         RefreshFields refreshFields = RefreshFields.getInstance();
-        System.out.println(refreshFields.getAddress().getStreet());
 
    }
 
